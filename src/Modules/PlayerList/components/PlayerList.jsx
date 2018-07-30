57617e9caab60f64bee3d6314d8cc99d9a967e0f
cristianmaxflores <cristianmaxflores@gmail.com>
2018-07-29 22:46:28 -0300

    constants y delete del codigo anterior

--- src/Modules/PlayerList/components/PlayerList.jsx
+++ src/Modules/PlayerList/components/PlayerList.jsx
@@ -2,27 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Segment, Container, Form, Header, Table } from 'semantic-ui-react'
 import PlayerListprops from '../../PlayerList'
-const { selectors, actions } = PlayerListprops
-
-const options = [
-  { key: '0', text: 'Position', value: '' },
-  { key: '1', text: 'Attacking Midfield', value: 'Attacking Midfield' },
-  { key: '2', text: 'Central Midfield', value: 'Central Midfield' },
-  { key: '3', text: 'Centre-Back', value: 'Centre-Back' },
-  { key: '4', text: 'Centre-Forward', value: 'Centre-Forward' },
-  { key: '5', text: 'Defensive Midfield', value: 'Defensive Midfield' },
-  { key: '6', text: 'Keeper', value: 'Keeper' },
-  { key: '7', text: 'Left Midfield', value: 'Left Midfield' },
-  { key: '8', text: 'Left Wing', value: 'Left Wing' },
-  { key: '9', text: 'Left-Back', value: 'Left-Back' },
-  { key: '10', text: 'Right-Back', value: 'Right-Back' },
-]
+const { selectors, actions, constants } = PlayerListprops
 
 class PlayerList extends React.Component {
 
   constructor() {
     super()
-    this.state = { playername: "", position: "", age: "" }//, arrayOfPlayers: [] }
+    this.state = { playername: "", position: "", age: "" }
   }
 
   handleChange = (e, { name, value }) => this.setState({ [name]: value })
@@ -39,7 +25,6 @@ class PlayerList extends React.Component {
       console.log("still fetching!")
   }
 
-
   getAge = (dateOfBirth) => {
     let today = new Date()
     let player = new Date(dateOfBirth)
@@ -54,41 +39,6 @@ class PlayerList extends React.Component {
       return age - 1
   }
 
-  // handleSubmit = () => {
-  //   //return console.log(this.props)
-  //   const { playername, position, age } = this.state
-  //   const { fetchedPlayers } = this.props
-  //   if (age === "" && position === "" && playername === "") {
-  //     console.log("wrong values! getting all players!")
-  //     return this.setState({ arrayOfPlayers: fetchedPlayers })
-  //   }
-  //   var newArrayOfPlayers = fetchedPlayers
-  //   if (age !== "") { newArrayOfPlayers = newArrayOfPlayers.filter(player => (this.getAge(player.dateOfBirth).toString() === age)) }
-  //   if (position !== "") { newArrayOfPlayers = newArrayOfPlayers.filter(player => (player.position === position)) }
-  //   if (playername !== "") { newArrayOfPlayers = newArrayOfPlayers.filter(player => (player.name.includes(playername))) }
-  //   this.setState({ arrayOfPlayers: newArrayOfPlayers })
-  //   return console.log(newArrayOfPlayers)
-  // }
-
-  // fetchPlayers = () => {
-  //   const { dispatch } = this.props;
-  //   console.log("fetching!")
-  //   dispatch(playerListActions.fetchPlayers())
-  // }
-
-  // componentWillMount() {
-  //   this.fetchPlayers()
-  // }
-
-  // componentWillReceiveProps(nextProps) {
-  //   console.log("nextProps")
-  //   console.log(nextProps)
-  //   if (nextProps.fetchedPlayers.length !== 0 && this.props.fetchedPlayers.length === 0) {
-  //     console.log("updating players")
-  //     this.setState({ arrayOfPlayers: nextProps.fetchedPlayers })
-  //   }
-  // }
-
   render() {
     const { status, loading, fetchedPlayers } = this.props
     const { playername, age } = this.state
@@ -99,7 +49,7 @@ class PlayerList extends React.Component {
           <Form onSubmit={this.handleFetch}>
             <Form.Group widths='equal' inline>
               <Form.Input name="playername" value={playername} onChange={this.handleChange} pattern="[ A-Za-z]+" title="Invalid Characters." fluid placeholder='Player Name' />
-              <Form.Select name="position" onChange={this.handleChange} fluid options={options} placeholder='Position' />
+              <Form.Select name="position" onChange={this.handleChange} fluid options={constants.FormPlayersPositions} placeholder='Position' />
               <Form.Input name="age" value={age} onChange={this.handleChange} type="number" min="18" max="40" fluid placeholder='Age' />
               <Form.Button loading={loading} type='submit'>Search</Form.Button>
             </Form.Group>
---
+++ src/Modules/PlayerList/playerList.constants.js
@@ -0,0 +1,13 @@
+export const FormPlayersPositions = [
+    { key: '0', text: 'Position', value: '' },
+    { key: '1', text: 'Attacking Midfield', value: 'Attacking Midfield' },
+    { key: '2', text: 'Central Midfield', value: 'Central Midfield' },
+    { key: '3', text: 'Centre-Back', value: 'Centre-Back' },
+    { key: '4', text: 'Centre-Forward', value: 'Centre-Forward' },
+    { key: '5', text: 'Defensive Midfield', value: 'Defensive Midfield' },
+    { key: '6', text: 'Keeper', value: 'Keeper' },
+    { key: '7', text: 'Left Midfield', value: 'Left Midfield' },
+    { key: '8', text: 'Left Wing', value: 'Left Wing' },
+    { key: '9', text: 'Left-Back', value: 'Left-Back' },
+    { key: '10', text: 'Right-Back', value: 'Right-Back' },
+  ]
