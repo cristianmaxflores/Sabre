5bb740645a20b45a4bab92a411fd2069053a1b02
cristianmaxflores <cristianmaxflores@gmail.com>
2018-07-31 12:39:24 -0300

    test PlayerList.Actions done

--- src/__tests__/PlayerList.actions.test.js
+++ src/__tests__/PlayerList.actions.test.js
@@ -6,22 +6,22 @@ import * as actionType from '../Modules/PlayerList/playerList.actionTypes'
 const mockStore = configureMockStore([thunk])
 const store = mockStore({ players: [], error: false, loading: false })
 const arrayFetched = [{ "name": "Romelu Lukaku", "nationality": "Belgium", "position": "Centre-Forward" }]
-const params = {name : "not relevant for the thunk"}
+const params = { name: "not relevant for the thunk" }
 const expectedActions = [
   { type: actionType.FETCH_PLAYERLIST_REQUEST },
   { type: actionType.FETCH_PLAYERLIST_SUCCESS, response: arrayFetched, params }
 ]
 
-function fetchPlayers(params) {
-  return dispatch => {
-    dispatch(request());
-    fetch('/someapi')
-      .then(
-        response => dispatch(success(response, params)),
-        error => dispatch(failure(error))
-      );
-  }
-}
+// function fetchPlayers(params) {
+//   return dispatch => {
+//     dispatch(request());
+//     fetch('/someapi')
+//       .then(
+//         response => dispatch(success(response, params)),
+//         error => dispatch(failure(error))
+//       );
+//   }
+// }
 function request() { return { type: actionType.FETCH_PLAYERLIST_REQUEST } }
 function success(response) { return { type: actionType.FETCH_PLAYERLIST_SUCCESS, response, params } }
 function failure(error) { return { type: actionType.FETCH_PLAYERLIST_FAILURE, error } }
@@ -48,15 +48,19 @@ describe('async actions', () => {
   })
   it('call fetchPlayers error', () => {
     fetchMock.get('*', { body: { players: arrayFetched }, headers: { 'content-type': 'application/json' } })
-    fetch().then(response => response.json())
+    fetch()
       .then(
-        response => new Promise.reject("some error...")//return a promise reject to trigger the error dispatch
+        response => {return Promise.reject("some error")}//return a promise reject to trigger the error dispatch
       )
       .then(
         response => store.dispatch(success(response, params)),
         error => store.dispatch(failure(error))
       );
   })
-
-})
+  it('check last action to be failure', () => {
+    const actualActions = store.getActions()
+    const failureAction = { type: actionType.FETCH_PLAYERLIST_FAILURE, error: "some error" }
+    expect(actualActions[2]).toEqual(failureAction)
+  })
+}) 
 
