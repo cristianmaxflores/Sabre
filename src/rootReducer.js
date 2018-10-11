34f5f718fa444d04b01cf1becfcc66bfeb2f0408
cristianmaxflores <35439949+cristianmaxflores@users.noreply.github.com>
2018-10-11 13:13:46 -0300

    bad route

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,4 +1,4 @@
-import { combineReducers } from '../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/redux'
+import { combineReducers } from 'redux'
 import PlayerList from './Modules/PlayerList'
 
 const appReducer = combineReducers({
